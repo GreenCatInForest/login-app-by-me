bdb608edb74d45d72749885f0e9c89c27d832b31
GreenCatInForest <event2016.2017@gmail.com>
2023-07-01 17:47:13 +0100

    add simple login form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+import React from "react";
+import { useState } from "react";
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>Login form</div>
+      <div>Enter your e-mail and password to log in, please </div>
+
+      <form>
+        <label>
+          E-mail
+          <input type="e-mail" name="e-mail" />
+        </label>
+        <label>
+          Password
+          <input type="text" name="password" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
